25a9f1ab557c532a2dae06f88d0087757427cd4d
lbenedette <blopesleo@outlook.com>
2015-09-07 02:12:34 -0300

    update sudoku.py file
    Creating functions to make sudoku graph and solve.

--- app/sudoku.py
+++ app/sudoku.py
@@ -1,156 +1,63 @@
 import networkx as nx
 
-#   1   2    3   4
-# |   | 1 || 4 |   |
-# | 2 |   ||   |   |
-# ------------------
-# |   | 3 ||   | 4 |
-# | 4 |   ||   | 1 |
-# create edges and verify the color and eliminate repeats
-g = nx.Graph()
+# networkx graph
+# g = nx.Graph()
+# g is a dict of dicts
+# g = {'node1': {'attr1': data1, 'attr2': data2}, 'node2': {}}
+# access node: g.node['node1']
+# access node attribute: g.node['node1']['attr1']
 
-# color = number
-# status = True : correct, False : not defined
+#TODO: create edge function
 
-# create nodes
-g.add_node(1, color=[1, 2, 3, 4], status=False)
-g.add_node(2, color=1, status=True)
-g.add_node(3, color=4, status=True)
-g.add_node(4, color=[1, 2, 3, 4], status=False)
+# node is a cell of sudoku
+# color: number
+# status:
+#   true: number correct
+#   false: not found
 
-g.add_node(5, color=2, status=True)
-g.add_node(6, color=[1, 2, 3, 4], status=False)
-g.add_node(7, color=[1, 2, 3, 4], status=False)
-g.add_node(8, color=[1, 2, 3, 4], status=False)
-
-g.add_node(9, color=[1, 2, 3, 4], status=False)
-g.add_node(10, color=3, status=True)
-g.add_node(11, color=[1, 2, 3, 4], status=False)
-g.add_node(12, color=4, status=True)
-
-g.add_node(13, color=4, status=True)
-g.add_node(14, color=[1, 2, 3, 4], status=False)
-g.add_node(15, color=[1, 2, 3, 4], status=False)
-g.add_node(16, color=1, status=True)
-
-# g.node is a dict - key = node name
-# to access data - [key = node name][key = data name]
-#print g.node[1]['status']
-#print g.node[1]['color']
-
-# create edges
-# square
-s1 = [(1, 2), (1, 5), (1, 6), (2, 5), (2, 6), (5, 6)]
-s2 = [(3, 4), (3, 7), (3, 8), (4, 7), (4, 8), (7, 8)]
-s3 = [(9, 10), (9, 13), (9, 14), (10, 13), (10, 14), (13, 14)]
-s4 = [(11, 12), (11, 15), (11, 16), (12, 15), (12, 16), (15, 16)]
-
-g.add_edges_from(s1)
-g.add_edges_from(s2)
-g.add_edges_from(s3)
-g.add_edges_from(s4)
-
-# lines
-l1 = [(1, 3), (1, 4), (2, 3), (2, 4)]
-l2 = [(5, 7), (5, 8), (6, 7), (6, 8)]
-l3 = [(9, 11), (9, 12), (10, 11), (10, 12)]
-l4 = [(13, 15), (13, 16), (14, 15), (14, 16)]
-
-g.add_edges_from(l1)
-g.add_edges_from(l2)
-g.add_edges_from(l3)
-g.add_edges_from(l4)
-
-# column
-c1 = [(1, 9), (1, 13), (5, 9), (5, 13)]
-c2 = [(2, 10), (2, 14), (6, 10), (6, 14)]
-c3 = [(3, 11), (3, 15), (7, 11), (7, 15)]
-c4 = [(4, 12), (4, 16), (8, 12), (8, 16)]
-
-g.add_edges_from(c1)
-g.add_edges_from(c2)
-g.add_edges_from(c3)
-g.add_edges_from(c4)
+# create nodes function
+# arg: graph: networkx graph
+#      data: matrix from web form
+# desc: create the nodes and add attributes using a matrix from web form.
+def CreateNode(graph, data):
+    i = 1
+    for j in range(9):
+        for k in range(9):
+            if data[j][k] > 0:
+                graph.add_node(i, color=data[j][k], status=True)
+            else:
+                graph.add_node(i, color=[1,2,3,4,5,6,7,8,9], status=False)
 
 
-# if node status is false
-# for all neighbors status true
-# try remove neighbor color
-for x in range(1,17):
-    if not g.node[x]['status']:
-        for e in g.neighbors(x):
-            if g.node[e]['status']:
-                try:
-                    g.node[x]['color'].remove(g.node[e]['color'])
-                # if the color is already removed
-                except ValueError:
-                    pass
-
-# update values
-for x in range(1, 17):
-    # status = false and one element in color list
-    if not g.node[x]['status'] and len(g.node[x]['color']) == 1:
-        g.node[x]['status'] = True
-        g.node[x]['color'] = g.node[x]['color'][0]
+# Welsh Powell graph coloring function
+# arg: networkx graph
+# desc: for all node, if status is false then verify the color of all neighbors
+# with status true and remove this color from your color list.
+def WelshPowell(graph):
+    for i in range(1,17):
+        if not graph.node[i]['status']:
+            for e in graph.neighbors(i):
+                if graph.node[e]['status']:
+                    # try remove color
+                    try:
+                        graph.node[i]['color'].remove(graph.node[e]['color'])
+                    # case color not on the list
+                    except ValueError:
+                        pass
 
 
-for x in range(1,17):
-    if not g.node[x]['status']:
-        for e in g.neighbors(x):
-            if g.node[e]['status']:
-                try:
-                    g.node[x]['color'].remove(g.node[e]['color'])
-                # if the color is already removed
-                except ValueError:
-                    pass
+# Update node value function
+# arg: networkx graph
+# desc: if status is false and color list lenght is 1, update status and color.
+def UpdateValue(graph):
+    for i in range(1, 17):
+        if not graph.node[i]['status'] and len(graph.node[i]['color']) == 1:
+            graph.node[i]['status'] = True
+            graph.node[i]['color'] = graph.node[i]['color'][0]
 
-# update values
-for x in range(1, 17):
-    # status = false and one element in color list
-    if not g.node[x]['status'] and len(g.node[x]['color']) == 1:
-        g.node[x]['status'] = True
-        g.node[x]['color'] = g.node[x]['color'][0]
 
-for x in range(1,17):
-    if not g.node[x]['status']:
-        for e in g.neighbors(x):
-            if g.node[e]['status']:
-                try:
-                    g.node[x]['color'].remove(g.node[e]['color'])
-                # if the color is already removed
-                except ValueError:
-                    pass
-
-# update values
-for x in range(1, 17):
-    # status = false and one element in color list
-    if not g.node[x]['status'] and len(g.node[x]['color']) == 1:
-        g.node[x]['status'] = True
-        g.node[x]['color'] = g.node[x]['color'][0]
-
-for x in range(1, 5):
-    if g.node[x]['status']:
-        print g.node[x]['color'],
-    else:
-        print 'x',
-print '\n'
-
-for x in range(5, 9):
-    if g.node[x]['status']:
-        print g.node[x]['color'],
-    else:
-        print 'x',
-print '\n'
-
-for x in range(9, 13):
-    if g.node[x]['status']:
-        print g.node[x]['color'],
-    else:
-        print 'x',
-print '\n'
-
-for x in range(13, 17):
-    if g.node[x]['status']:
-        print g.node[x]['color'],
-    else:
-        print 'x',
+#TODO: doing!!!
+def SudokuSolution(data):
+    # create graph using networkx lib
+    sudoku = nx.Graph()
+    CreateNode(sudoku, data)
