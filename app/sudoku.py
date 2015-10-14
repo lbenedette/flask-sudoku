56f2f5d6cdbcea4e038c2d049f93306ac2c4be79
Leonardo Benedette Lopes <blopesleo@outlook.com>
2015-10-14 20:01:41 -0300

    Sudoku 9x9 solution
    It remains to implement the relationships between nodes in the same
    square.

--- app/sudoku.py
+++ app/sudoku.py
@@ -1,5 +1,6 @@
 import networkx as nx
 
+
 # networkx graph
 # g = nx.Graph()
 # g is a dict of dicts
@@ -7,7 +8,13 @@ import networkx as nx
 # access node: g.node['node1']
 # access node attribute: g.node['node1']['attr1']
 
-#TODO: create edge function
+number = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+nodes = []
+for i in range(len(number)):
+    nodes.append([])
+    for j in range(len(number)):
+        nodes[i].append(number[i]+number[j])
+
 
 # node is a cell of sudoku
 # color: number
@@ -19,45 +26,57 @@ import networkx as nx
 # arg: graph: networkx graph
 #      data: matrix from web form
 # desc: create the nodes and add attributes using a matrix from web form.
-def CreateNode(graph, data):
-    i = 1
-    for j in range(9):
-        for k in range(9):
-            if data[j][k] > 0:
-                graph.add_node(i, color=data[j][k], status=True)
+def createNode(graph, data):
+    for i in range(9):
+        for j in range(9):
+            if int(data[nodes[i][j]]) > 0:
+                graph.add_node(nodes[i][j], color=int(data[nodes[i][j]]), status=True)
             else:
-                graph.add_node(i, color=[1,2,3,4,5,6,7,8,9], status=False)
+                graph.add_node(nodes[i][j], color=[1,2,3,4,5,6,7,8,9], status=False)
+
+
+def createEdge(graph):
+    for i in range(len(number)):
+        for j in range(len(number)-1):
+            for k in range(j, len(number)-1):
+                graph.add_edge(nodes[i][j], nodes[i][k+1])
+
+    for i in range(len(number)-1):
+        for j in range(len(number)):
+            for k in range(i, len(number)-1):
+                graph.add_edge(nodes[i][j], nodes[k+1][j])
 
 
 # Welsh Powell graph coloring function
 # arg: networkx graph
 # desc: for all node, if status is false then verify the color of all neighbors
 # with status true and remove this color from your color list.
-def WelshPowell(graph):
-    for i in range(1,17):
-        if not graph.node[i]['status']:
-            for e in graph.neighbors(i):
-                if graph.node[e]['status']:
-                    # try remove color
-                    try:
-                        graph.node[i]['color'].remove(graph.node[e]['color'])
-                    # case color not on the list
-                    except ValueError:
-                        pass
+def welshPowell(graph):
+    for i in range(len(number)):
+        for j in range(len(number)):
+            if not graph.node[nodes[i][j]]['status']:
+                for e in graph.neighbors(nodes[i][j]):
+                    if graph.node[e]['status']:
+                        # try remove color
+                        try:
+                            graph.node[nodes[i][j]]['color'].remove(graph.node[e]['color'])
+                        # case color not on the list
+                        except ValueError:
+                            pass
 
 
 # Update node value function
 # arg: networkx graph
-# desc: if status is false and color list lenght is 1, update status and color.
-def UpdateValue(graph):
-    for i in range(1, 17):
-        if not graph.node[i]['status'] and len(graph.node[i]['color']) == 1:
-            graph.node[i]['status'] = True
-            graph.node[i]['color'] = graph.node[i]['color'][0]
+# desc: if status is false and color list length is 1, update status and color.
+def updateValue(graph):
+    for i in range(len(number)):
+        for j in range(len(number)):
+            if not graph.node[nodes[i][j]]['status'] and len(graph.node[nodes[i][j]]['color']) == 1:
+                graph.node[nodes[i][j]]['status'] = True
+                graph.node[nodes[i][j]]['color'] = graph.node[nodes[i][j]]['color'][0]
 
 
-#TODO: doing!!!
-def SudokuSolution(data):
-    # create graph using networkx lib
-    sudoku = nx.Graph()
-    CreateNode(sudoku, data)
+def makeSolution(graph):
+    for i in range(5):
+        welshPowell(graph)
+        updateValue(graph)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,18 @@
-from flask import render_template
+from flask import render_template, request
+from sudoku import nx, nodes, createNode, createEdge, makeSolution
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', nodes=nodes)
+
+
+@app.route("/solution", methods=['POST'])
+def sudoku():
+    sdk = nx.Graph()
+    data = request.form
+    createNode(sdk, data)
+    createEdge(sdk)
+    makeSolution(sdk)
+    return render_template('solution.html', nodes=nodes, graph=sdk)
