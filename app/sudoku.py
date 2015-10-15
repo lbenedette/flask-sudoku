73eb1c0c10f916bed39e8e96651bb2c5cc05b591
Leonardo Benedette Lopes <blopesleo@outlook.com>
2015-10-14 22:17:43 -0300

    Update sudoku.py
    Now the function createEdge create edge of nodes from the same square.

--- app/sudoku.py
+++ app/sudoku.py
@@ -15,6 +15,16 @@ for i in range(len(number)):
     for j in range(len(number)):
         nodes[i].append(number[i]+number[j])
 
+square = [['00','01','02','10','11','12','20','21','22'],
+          ['03','04','05','13','14','15','23','24','25'],
+          ['06','07','08','16','17','18','26','27','28'],
+          ['30','31','32','40','41','42','50','51','52'],
+          ['33','34','35','43','44','45','53','54','55'],
+          ['36','37','38','46','47','48','56','57','58'],
+          ['60','61','62','70','71','72','80','81','82'],
+          ['63','64','65','73','74','75','83','84','85'],
+          ['66','67','68','76','77','78','86','87','88']]
+
 
 # node is a cell of sudoku
 # color: number
@@ -45,6 +55,11 @@ def createEdge(graph):
         for j in range(len(number)):
             for k in range(i, len(number)-1):
                 graph.add_edge(nodes[i][j], nodes[k+1][j])
+
+    for i in range(len(number)):
+        for j in range(len(number)-1):
+            for k in range(j, len( number)-1):
+                graph.add_edge(square[i][j], square[i][k+1])
 
 
 # Welsh Powell graph coloring function
